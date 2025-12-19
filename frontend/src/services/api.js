78da8055ae36fcb25004154e46bd645bfef0fe23
avani241 <avani24112001@gmail.com>
2025-12-20 03:49:40 +0530

    Update API base URL for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+const api = axios.create({
+  baseURL: "https://landing-page-1239.onrender.com/api",
 });
 
-export const fetchProjects = () => API.get("/projects");
-export const fetchClients = () => API.get("/clients");
-
-export const addProject = (data) => API.post("/projects", data);
-export const addClient = (data) => API.post("/clients", data);
-
-export const submitContact = (data) => API.post("/contact", data);
-export const fetchContacts = () => API.get("/contact");
-
-export const subscribeNewsletter = (data) => API.post("/newsletter");
-export const fetchSubscribers = () => API.get("/newsletter");
+export default api;
